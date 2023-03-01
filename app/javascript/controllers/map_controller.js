d47244c73abff1729c0158e7e38037a90a05ac77
RobertaFreitas <robertaefreitas@gmail.com>
2023-03-01 15:51:15 +0100

    map inserted

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,6 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="map"
 export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
 
