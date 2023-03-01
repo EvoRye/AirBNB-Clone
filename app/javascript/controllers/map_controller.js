2d2b4e6323721a7d770bf911fe6fe65732cd3d65
RobertaFreitas <robertaefreitas@gmail.com>
2023-03-01 16:07:13 +0100

    hacking

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,5 +1,43 @@
 import { Controller } from "@hotwired/stimulus"
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Connects to data-controller="map"
 export default class extends Controller {
   static values = {
